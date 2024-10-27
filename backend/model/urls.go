ce24b1cdd5ba7c3830f2366f596bfe648e3b2d98
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-27 22:48:18 +0530

    fix: fixed the type for User_id to `string`

--- backend/model/urls.go
+++ backend/model/urls.go
@@ -4,7 +4,7 @@ import "time"
 
 type UrlModel struct {
 	Uid        int       `json:"uid"`
-	User_id    int       `json:"user_id"`
+	User_id    string    `json:"user_id"`
 	Url        string    `json:"url"`
 	Created_at time.Time `json:"created_at"`
 	Clicks     int       `json:"clicks"`
