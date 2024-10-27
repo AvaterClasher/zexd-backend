ce7433a3d554731d355078056e14624a1a5a405d
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-27 22:39:01 +0530

    feat: Added dao for inserting shortened url

---
+++ backend/daos/random_insert.go
@@ -0,0 +1,44 @@
+package daos
+
+import (
+	"database/sql"
+	"log"
+	database "zexd/db"
+	"zexd/model"
+
+	_ "github.com/lib/pq"
+)
+
+var DB_NAME string
+var TABLE_NAME string
+var db *sql.DB
+
+func init() {
+	db = database.CreateCon()
+
+	createTableQuery := `
+		CREATE TABLE IF NOT EXISTS shortened_url (
+			uid SERIAL PRIMARY KEY,
+			user_id TEXT NOT NULL,
+			url TEXT NOT NULL,
+			created_at TIMESTAMP DEFAULT NOW(),
+			clicks INT DEFAULT 0
+		)
+	`
+
+	_, err := db.Exec(createTableQuery)
+	if err != nil {
+		log.Fatalf("Error creating table: %v", err)
+	}
+}
+
+func InsertShortenedUrl(urlModel model.UrlModel) (int, error) {
+	var uid int
+	query := "INSERT INTO shortened_url (url,user_id,created_at,clicks) VALUES ($1,$2,NOW(),0) RETURNING uid"
+	err := db.QueryRow(query, urlModel.Url, urlModel.User_id).Scan(&uid)
+	if err != nil {
+		log.Println("Error inserting shortened URL:", err)
+		return 0, err
+	}
+	return uid, nil
+}
