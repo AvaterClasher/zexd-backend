fb974f68f8aece31b691dc7e09e7da64506d3ddb
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-29 00:46:26 +0530

    feat: Added the cronjob in goroutine in the main func

--- backend/main.go
+++ backend/main.go
@@ -4,13 +4,24 @@ import (
 	"log"
 	"net/http"
 	"zexd/handlers"
+	"zexd/services"
+
+	"github.com/jasonlvhit/gocron"
 )
+
+func executeCronJob() {
+	log.Println("**Executing Cron Service**")
+	gocron.Every(60).Minute().Do(services.RemoveExpiredEntries)
+	<-gocron.Start()
+}
 
 func main() {
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: handlers.New(),
 	}
+
+	go executeCronJob()
 
 	log.Printf("Starting server on %s", server.Addr)
 	if err := server.ListenAndServe(); err != http.ErrServerClosed {
