7d135794e22677c075f68f820840e6c430e27d21
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-29 13:44:16 +0530

    feat: Now url gets added to cache on creation for future use

--- backend/services/url_creation.go
+++ backend/services/url_creation.go
@@ -2,16 +2,26 @@ package services
 
 import (
 	"encoding/base64"
+	"log"
 	"os"
 	"strconv"
+	"time"
+	"zexd/cache"
 	"zexd/daos"
 	"zexd/model"
+
+	"github.com/redis/go-redis/v9"
 )
 
 var url string
 var set bool
 
+var ctx = cache.Ctx
+
+var rdb *redis.Client
+
 func init() {
+	rdb = cache.CreateCon()
 	url, set = os.LookupEnv("SERVER_DOMAIN")
 	if !set {
 		url = "http://localhost:8080/"
@@ -26,6 +36,11 @@ func CreateShortenedUrl(inputUrl string, user_id string) string {
 	byteNumber := []byte(strconv.Itoa(uid))
 	tempUrl := base64.StdEncoding.EncodeToString(byteNumber)
 	newUrl := url + tempUrl
-	
+
+	err = rdb.Set(ctx, tempUrl, inputUrl, time.Duration(exp_time)*time.Minute).Err()
+	if err != nil {
+		log.Printf("Error in setting Redis value: %v", err)
+	}
+
 	return newUrl
 }
