d564143550cc042e90b7110dd4c8c5b326ea984f
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-29 13:45:52 +0530

    feat: Original url can now be extracted from the cache itself without db call
    A seperate go routine is called to increment the click so that it is non-blocking and the user gets the full benefit of the cache.

--- backend/services/get_url.go
+++ backend/services/get_url.go
@@ -6,15 +6,33 @@ import (
 	"strconv"
 	"strings"
 	"zexd/daos"
+
+	"github.com/redis/go-redis/v9"
 )
 
 func UrlRedirection(shortenedUrl string) (string, error) {
+	inputUrl, err := rdb.Get(ctx, shortenedUrl).Result()
+	if err == nil {
+		log.Println("Shortened URL found in cache", inputUrl)
+		encodedPart := shortenedUrl[strings.LastIndex(shortenedUrl, "/")+1:]
+		byteNumber, _ := base64.StdEncoding.DecodeString(encodedPart)
+		uid, _ := strconv.Atoi(string(byteNumber))
+		go func() {
+			if dbErr := daos.IncrementClickCount(uid); dbErr != nil {
+				log.Println("Error incrementing click count:", dbErr)
+			}
+		}()
+		return inputUrl, nil
+	} else if err != redis.Nil {
+		log.Println("Redis error:", err)
+	}
 
 	encodedPart := shortenedUrl[strings.LastIndex(shortenedUrl, "/")+1:]
 	byteNumber, _ := base64.StdEncoding.DecodeString(encodedPart)
 	uid, _ := strconv.Atoi(string(byteNumber))
 
-	inputUrl, err := daos.GetUrlAndIncrement(uid)
+	inputUrl, err = daos.GetUrlAndIncrement(uid)
+	log.Println("URL from database", inputUrl)
 	if err != nil {
 		log.Println("Error in getting URL from database", err)
 		return "", err
