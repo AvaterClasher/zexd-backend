17fa15380a34a385cfd4b03e07b92d1965f42cb8
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-30 22:14:59 +0530

    feat: Updated log package for inc clicks

--- backend/daos/increment_click.go
+++ backend/daos/increment_click.go
@@ -1,11 +1,9 @@
 package daos
 
-import "log"
-
 func IncrementClickCount(uid int) error {
 	_, err := db.Exec("UPDATE shortened_url SET clicks = clicks + 1 WHERE uid = $1", uid)
 	if err != nil {
-		log.Println("Error updating click count:", err)
+		log.Errorf("Error updating click count: %v", err)
 		return err
 	}
 	return nil
