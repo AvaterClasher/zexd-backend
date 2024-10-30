0d2ea73aa8afed37bf24442e49a964679df06e5b
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-30 18:46:21 +0530

    feat: Added docs comments and setup swagger to generate docs

--- backend/handlers/route.go
+++ backend/handlers/route.go
@@ -2,13 +2,33 @@ package handlers
 
 import (
 	"net/http"
+
 	"github.com/gorilla/mux"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
+	_ "zexd/docs"
 )
 
+// Swagger Info
+// @title ZexD API
+// @version 1.0
+// @description API for a URL Shortener Service.
+
+// @license.name MIT
+// @license.url https://opensource.org/license/mit
+
+// @host localhost:8080
+// @BasePath /
 func New() http.Handler {
 	route := mux.NewRouter()
-	route.HandleFunc("/check", BasicHandler)
+	route.HandleFunc("/health", HealthCheckHandler)
 	mainRouter := route.PathPrefix("/").Subrouter()
+
+	mainRouter.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
+		httpSwagger.URL("http://localhost:8080/swagger/doc.json"),
+		httpSwagger.DeepLinking(true),
+		httpSwagger.DocExpansion("none"),
+		httpSwagger.DomID("swagger-ui"),
+	)).Methods(http.MethodGet)
 
 	mainRouter.HandleFunc("/{shortenedUrl}", RedirectHandler)
 	mainRouter.HandleFunc("/api/create", CreateUrlHandler).Methods("POST", "OPTIONS")
