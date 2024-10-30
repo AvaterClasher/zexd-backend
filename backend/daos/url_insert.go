100a18a9d49b6f9ea97376d19be21a5ce1f06dce
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-30 22:13:52 +0530

    feat: Updated log package for url insert

--- backend/daos/url_insert.go
+++ backend/daos/url_insert.go
@@ -2,8 +2,8 @@ package daos
 
 import (
 	"database/sql"
-	"log"
 	database "zexd/db"
+	"zexd/logger"
 	"zexd/model"
 
 	_ "github.com/lib/pq"
@@ -12,6 +12,8 @@ import (
 var DB_NAME string
 var TABLE_NAME string
 var db *sql.DB
+
+var log = logger.NewLogger()
 
 func init() {
 	db = database.CreateCon()
@@ -37,7 +39,7 @@ func InsertShortenedUrl(urlModel model.UrlModel) (int, error) {
 	query := "INSERT INTO shortened_url (url,user_id,created_at,clicks) VALUES ($1,$2,NOW(),0) RETURNING uid"
 	err := db.QueryRow(query, urlModel.Url, urlModel.User_id).Scan(&uid)
 	if err != nil {
-		log.Println("Error inserting shortened URL:", err)
+		log.Errorf("Error inserting shortened URL: %v", err)
 		return 0, err
 	}
 	return uid, nil
