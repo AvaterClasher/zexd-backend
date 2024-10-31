b7b428d4d8a24d5c5f1721fc27dbf332aca83e92
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-31 19:01:31 +0530

    fix: changed fatal to fatalf as args need to be passed

--- backend/cache/cache_conn.go
+++ backend/cache/cache_conn.go
@@ -16,7 +16,7 @@ var log = logger.NewLogger()
 func init() {
 	envFile := ".env"
 	if err := godotenv.Load(envFile); err != nil {
-		log.Fatal("Error: No Environment File Found, cache_connection.go", err)
+		log.Fatalf("Error: No Environment File Found, cache_connection.go %v", err)
 	}
 }
 
