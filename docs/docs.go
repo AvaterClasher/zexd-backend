8b00583e739be9f90007c7c95176bf2e9511c9b4
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-11-01 00:17:31 +0530

    feat: Updated docs for hosting

--- docs/docs.go
+++ docs/docs.go
@@ -239,7 +239,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "https://zexd.onrender.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "ZexD API",
