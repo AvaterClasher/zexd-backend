68409b2bb8fb1ff446cb13c3d13a1f56967c5ef7
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-10-31 12:31:41 +0530

    feat: Added middlware for request logging

---
+++ backend/middleware/log_middleware.go
@@ -0,0 +1,15 @@
+package middleware
+
+import (
+	"net/http"
+	"zexd/logger"
+)
+
+var log = logger.NewLoggerforHttp()
+
+func LogMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Infof("Request: %s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
